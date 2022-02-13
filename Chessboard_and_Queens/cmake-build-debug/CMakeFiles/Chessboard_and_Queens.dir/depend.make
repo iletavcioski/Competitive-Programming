6c44e183cd1cf0f8e5d5e3c537baf1a5b72fb0b8
tavchija <it8816@student.uni-lj.si>
2022-02-13 23:38:16 +0100

    kaizen day 1

--- Chessboard_and_Queens/cmake-build-debug/CMakeFiles/Chessboard_and_Queens.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Chessboard_and_Queens.
-# This may be replaced when dependencies are built.
--- Chessboard_and_Queens/main.cpp
+++ Chessboard_and_Queens/main.cpp
@@ -1,45 +1,91 @@
 #include <iostream>
 
 char board[8][8];
-bool visited[8];
+int vis[8][8];
+int right[8][8];
+int left[8][8];
+int nright[8][8];
+int nleft[8][8];
+int nvis[8][8];
 
-void read() {
+int res = 0;
+
+void rec(int pos, int k) {
+    //std::cout << "<--------------->\n";
+    if (k == 0) {
+        res++;
+        return;
+    }
+
+
+    if (8 - k > 0) {
+        for (int i = 7; i >= 1; i--) {
+            right[8 - k][i] = right[8 - k - 1][i - 1];
+        }
+        right[8 - k][0] = 0;
+
+        for (int i = 0; i < 7; i++) {
+            left[8 - k][i] = left[8 - k - 1][i + 1];
+        }
+        left[8 - k][7] = 0;
+
+        for (int i = 0; i < 8; i++) {
+            vis[8 - k][i] = vis[8 - k - 1][i];
+        }
+    }
+
+
+    /*
+    std::cout << "right\n";
+    for (int i = 0; i < 8; i++) {
+        std::cout << right[8 - k][i] << ' ';
+    }
+    std::cout << '\n';
+    std::cout << "left\n";
+    for (int i = 0; i < 8; i++) {
+        std::cout << left[8 - k][i] << ' ';
+    }
+    std::cout << '\n';
+    std::cout << "vis\n";
+    for (int i = 0; i < 8; i++) {
+        std::cout << vis[8 - k][i] << ' ';
+    }
+    std::cout << '\n';
+    */
+    for (int i = 0; i < 8; i++) {
+        if (!vis[8 - k][i] && !right[8 - k][i] && !left[8 - k][i] && board[8 - k][i] == '.') {
+            for (int j = 0; j < 8; j++) {
+                nright[8 - k][j] = right[8 - k][j];
+                nleft[8 - k][j] = left[8 - k][j];
+                nvis[8 - k][j] = vis[8 - k][j];
+            }
+            vis[8 - k][i] = 1;
+            right[8 - k][i] = 1;
+            left[8 - k][i] = 1;
+            rec(i, k - 1);
+            for (int j = 0; j < 8; j++) {
+                right[8 - k][j] = nright[8 - k][j];
+                left[8 - k][j] = nleft[8 - k][j];
+                vis[8 - k][j] = nvis[8 - k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie();
+    std::cout.tie();
+
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
             std::cin >> board[i][j];
         }
     }
-}
 
-void init() {
-    for (int i = 0; i < 8; i++) {
-        visited[i] = (board[0][i] == '*');
-    }
-}
+    rec(-1, 8);
+    std::cout << res << '\n';
 
-int solve(int row, int pos) {
-    if (row == 7)
-        return 1;
 
-    int cnt = 0;
-    for (int i = 0; i < 8; i++) {
-        if (!visited[i]) {
-            std::vector<int> positions;
-            if (i - 1 >= 0)
-                visited[i - 1] = true;
-            if (i + 1 < 8)
-                visited[i + 1] = true;
-            visited[i] = true;
-            cnt += solve(row + 1, i);
-        }
-    }
-
-    return cnt;
-}
-
-int main() {
-    read();
-    init();
-    std::cout << solve() << std::endl;
     return 0;
 }
