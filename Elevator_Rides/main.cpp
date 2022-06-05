a58130c235207bc82b6cbf7e55b84c98a76c046f
teletton <it8816@student.uni-lj.si>
2022-06-05 13:40:20 +0200

    elevator rides solved

--- Elevator_Rides/main.cpp
+++ Elevator_Rides/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 struct state {
-    int bm;
-    int val;
-    int len;
+    int bitmask;
+    long long weight;
+    int steps;
 };
+
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie();
@@ -21,39 +23,47 @@ int main() {
     for (int i = 0; i < n; i++) {
         std::cin >> ar[i];
     }
+    std::sort(ar.begin(), ar.end());
+    int dp[1 << n];
+    int dp2[1 << n];
 
-    int dp[n][1 << n];
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < (1 << n); j++) {
-            dp[i][j] = 1e9;
-        }
+    for (int i = 0; i < (1 << n); i++) {
+        dp[i] = 1e9;
+        dp2[i] = 1e9;
     }
-    dp[0][0] = 0;
-    std::queue<state> q;
-    q.push({0, 0, 0});
-    int res = n - 1;
-    while (!q.empty()) {
-        auto p = q.front();
-        q.pop();
 
-        if (p.bm == (1 << n) - 1) {
-            res = std::min(res, p.len);
+    std::queue<state> qi;
+    dp[0] = 1;
+    dp2[0] = 0;
+    qi.push({0, 0, 1});
+    while (!qi.empty()) {
+        state topi = qi.front();
+        qi.pop();
+
+        if (dp[topi.bitmask] == topi.steps && dp2[topi.bitmask] < topi.weight) {
+            continue;
         }
-
         for (int i = 0; i < n; i++) {
-            if (!((1 << i) & p.bm)) {
-                if (dp[p.len][p.bm | (1 << i)] > p.val + ar[i] && p.val + ar[i] <= m) {
-                    dp[p.len][p.bm | (1 << i)] = p.val + ar[i];
-                    q.push({p.bm | (1 << i), p.val + ar[i], p.len});
-                } else if (dp[p.len + 1][p.bm | (1 << i)] > ar[i] && p.val + ar[i] > m) {
-                    dp[p.len + 1][p.bm | (1 << i)] = ar[i];
-                    q.push({p.bm | (1 << i), ar[i], p.len + 1});
-                }
+            if (ar[i] + topi.weight <= m && !((1 << i) & topi.bitmask) && dp[(1 << i) | topi.bitmask] > topi.steps) {
+                dp[topi.bitmask | (1 << i)] = topi.steps;
+                dp2[topi.bitmask | (1 << i)] = topi.weight + ar[i];
+                qi.push({topi.bitmask | (1 << i), ar[i] + topi.weight, topi.steps});
+            } else if (ar[i] <= m && !((1 << i) & topi.bitmask) && dp[(1 << i) | topi.bitmask] > topi.steps + 1) {
+                dp[topi.bitmask | (1 << i)] = topi.steps + 1;
+                dp2[topi.bitmask | (1 << i)] = ar[i];
+                qi.push({topi.bitmask | (1 << i), ar[i], topi.steps + 1});
+            } else if (ar[i] + topi.weight <= m && !((1 << i) & topi.bitmask) && dp[(1 << i) | topi.bitmask] == topi.steps && dp2[(1 << i) | topi.bitmask] > ar[i] + topi.weight) {
+                dp[topi.bitmask | (1 << i)] = topi.steps;
+                dp2[topi.bitmask | (1 << i)] = topi.weight + ar[i];
+                qi.push({topi.bitmask | (1 << i), ar[i] + topi.weight, topi.steps});
+            } else if (ar[i] <= m && !((1 << i) & topi.bitmask) && dp[(1 << i) | topi.bitmask] == topi.steps + 1 &&  dp2[(1 << i) | topi.bitmask] > ar[i]) {
+                dp[topi.bitmask | (1 << i)] = topi.steps + 1;
+                dp2[topi.bitmask | (1 << i)] = ar[i];
+                qi.push({topi.bitmask | (1 << i), ar[i], topi.steps + 1});
             }
         }
-
     }
-    std::cout << res + 1 << '\n';
 
+    std::cout << dp[(1 << n) - 1] << "\n";
     return 0;
 }
