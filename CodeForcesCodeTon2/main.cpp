6f5a79196bdb4e583da35dcaf04985d6066b5783
theteleton <it8816@student.uni-lj.si>
2022-10-07 11:33:14 +0200

    last thing

---
+++ CodeForcesCodeTon2/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie();
+    std::cout.tie();
+
+    int t;
+    std::cin >> t;
+
+    while (t--) {
+
+    }
+    return 0;
+}
+
+/*
+1
+3
+3 3 2
+ */
