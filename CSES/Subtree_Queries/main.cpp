ce5b4f9c6007d0219ae721914568193e75f3877a
theteleton <it8816@student.uni-lj.si>
2023-05-16 16:18:32 +0200

    subtree queries done

--- CSES/Subtree_Queries/main.cpp
+++ CSES/Subtree_Queries/main.cpp
@@ -21,11 +21,11 @@ void dfs(int cur, int prev) {
 
 template<typename T>
 class node {
-    public:
+public:
     node* left;
     node* right;
     T value;
-    
+
     node() {
         this->left = NULL;
         this->right = NULL;
@@ -35,7 +35,7 @@ class node {
 
 template<typename T>
 class segmentTree {
-    public:
+public:
     node<T>* root;
     std::vector<T> numbers;
 
@@ -47,6 +47,7 @@ class segmentTree {
     void build(node<T>* current_node, int left, int right) {
         if (left == right) {
             current_node->value = this->numbers[left];
+            return;
         }
 
         current_node->left = new node<T>();
@@ -68,6 +69,7 @@ class segmentTree {
 
         update(current_node->left, position, value, left, (left + right) / 2);
         update(current_node->right, position, value, (left + right) / 2 + 1, right);
+        current_node->value = current_node->left->value + current_node->right->value;
     }
 
     T query(node<T>* current_node, int left_range, int right_range, int left, int right) {
@@ -91,7 +93,7 @@ int main() {
     int n, q;
     std::cin >> n >> q;
 
-    
+
     tree_values.resize(n);
     std::vector<int> vec;
     tree.insert(tree.begin(), n, vec);
@@ -110,9 +112,9 @@ int main() {
     }
 
     dfs(0, -1);
-    int sz = tree_values.size();
+    int sz = array.size();
     auto *root = new node<long long>();
-    auto *obj = new segmentTree<long long>(root, &tree_values);
+    auto *obj = new segmentTree<long long>(root, &array);
     obj->build(obj->root, 0, sz - 1);
 
     for (int i = 0; i < q; i++) {
@@ -123,14 +125,14 @@ int main() {
             int s, value;
             std::cin >> s >> value;
             s--;
-
+            //std::cout << map[s].first << " " << map[s].second << std::endl;
             obj->update(obj->root, map[s].first, value, 0, sz - 1);
             obj->update(obj->root, map[s].second, value, 0, sz - 1);
         } else {
             int s;
             std::cin >> s;
             s--;
-            std::cout << obj->query(obj->root, map[s].first, map[s].second, 0, sz - 1) << "\n";
+            std::cout << obj->query(obj->root, map[s].first, map[s].second, 0, sz - 1) / 2 << "\n";
         }
     }
 
