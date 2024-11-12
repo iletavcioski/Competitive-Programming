864ab7601d7340351ff4f11ff1936e07427c1661
theteleton <it8816@student.uni-lj.si>
2024-11-12 10:29:45 +0100

    Kaizen day 2

--- CSES/Path_Queries/main.cpp
+++ CSES/Path_Queries/main.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 
 // Fenwick Tree
-void update(std::vector<int> &fenwick_tree, int index, int value) {
+void update(std::vector<long long> &fenwick_tree, int index, long long value) {
     while (index < fenwick_tree.size()) {
         fenwick_tree[index] += value;
-        index += index & (-index); 
+        index += index & (-index);
     }
 }
 
-long long query(std::vector<int> &fenwick_tree, int index) {
+long long query(std::vector<long long> &fenwick_tree, int index) {
     long long sum = 0;
     while (index > 0) {
         sum += fenwick_tree[index];
@@ -18,15 +18,28 @@ long long query(std::vector<int> &fenwick_tree, int index) {
     return sum;
 }
 
-std::vector<int> euler_path;
-void dfs(std::vector<std::vector<int> > &tree, int current_node, int previous_node) {
-    euler_path.push_back(current_node);
+std::vector<int> values;
+std::vector<long long> euler_path;
 
+void dfs(std::vector<std::vector<int> > &tree, std::vector<int> &_map, int current_node, int previous_node, long long total_sum) {
+    euler_path.push_back(total_sum);
+    _map[current_node] = euler_path.size() - 1;
     for (int next_node : tree[current_node]) {
-        if (current_node == previous_node) {
-            dfs(tree, next_node, current_node);
+        if (next_node != previous_node) {
+            dfs(tree, _map, next_node, current_node, total_sum + values[next_node]);
         }
     }
+}
+
+int subtree_dfs(std::vector<int> &subtree_size, std::vector<std::vector<int> > &tree, int current_node, int previous_node) {
+    subtree_size[current_node] = 1;
+
+    for (int next_node : tree[current_node]) {
+        if (next_node != previous_node) {
+            subtree_size[current_node] += subtree_dfs(subtree_size, tree, next_node, current_node);
+        }
+    }
+    return subtree_size[current_node];
 }
 
 int main() {
@@ -37,13 +50,16 @@ int main() {
     int n, q;
     std::cin >> n >> q;
 
-    std::vector<int> values(0, n);
 
     for (int i = 0; i < n; i++) {
-        std::cin >> values[i];
+        int a;
+        std::cin >> a;
+        values.push_back(a);
     }
 
-    std::vector<int> tree[n];
+    std::vector<std::vector<int> > tree;
+    std::vector<int> vec;
+    tree.insert(tree.begin(), n, vec);
     for (int i = 0; i < n - 1; i++) {
         int a, b;
         std::cin >> a >> b;
@@ -52,8 +68,39 @@ int main() {
         tree[a].push_back(b);
         tree[b].push_back(a);
     }
+    std::vector<int> subtree_size(n, 0);
 
-    
+    std::vector<long long> fenwick_tree(n + 1, 0);
+
+    std::vector<int> _map(n, 0);
+    dfs(tree, _map, 0, -1, values[0]);
+    subtree_dfs(subtree_size, tree, 0, -1);
+
+    for (int i = 0; i < n; i++) {
+        update(fenwick_tree, _map[i] + 1, values[i]);
+        update(fenwick_tree, _map[i] + subtree_size[i] + 1, -values[i]);
+    }
+
+    for (int i = 0; i < q; i++) {
+        int query_type;
+        std::cin >> query_type;
+
+        if (query_type == 1) {
+            int s, x;
+            std::cin >> s >> x;
+            s--;
+
+            long long difference = x - values[s];
+            update(fenwick_tree, _map[s] + 1, difference);
+            update(fenwick_tree, _map[s] + 1 + subtree_size[s], -difference);
+            values[s] = x;
+        } else {
+            int s;
+            std::cin >> s;
+            s--;
+            std::cout << query(fenwick_tree, _map[s] + 1)  << "\n";
+        }
+    }
 
 
 
