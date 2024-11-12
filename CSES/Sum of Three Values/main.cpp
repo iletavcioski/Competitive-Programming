8d82da19e807ced9c8713d0f73499b8230ff5698
theteleton <it8816@student.uni-lj.si>
2024-11-12 22:33:13 +0100

    Kaizen day 2, Sum of Three Values solved

--- CSES/Sum of Three Values/main.cpp
+++ CSES/Sum of Three Values/main.cpp
@@ -10,28 +10,35 @@ int main() {
     int n, x;
     std::cin >> n >> x;
     std::unordered_map<int, int> map;
+    
     int array[5001];
-    int arr[5001];
+    std::pair<int,int> arr[5001];
     for (int i = 0; i < n; i++) {
         std::cin >> array[i];
         map[array[i]] = i + 1;
-        arr[i] = array[i];
+        arr[i] = {array[i], i + 1};
     }
 
     std::sort(arr, arr + n);
-    if ((long long)x > (long long)arr[n - 1] + (long long)arr[n - 2] + (long long)arr[n - 3]) {
-        std::cout << "IMPOSSIBLE" << std::endl;
+    if ((long long)x > (long long)arr[n - 1].first + (long long)arr[n - 2].first + (long long)arr[n - 3].first) {
+        std::cout << "IMPOSSIBLE" << "\n";
         return 0;
     }
     for (int i = 0; i < n; i++) {
-        int num = x - array[i];
+        int num = x - arr[i].first;
+        if (num < 0) {
+            continue;
+        }
         for (int j = i + 1; j < n; j++) {
-            if (map[num - array[j]] && map[num - array[j]] != i + 1 && map[num - array[j]] != j + 1) {
-                std::cout << map[x - array[i] - array[j]] << " " << i + 1 << " " << j + 1 << std::endl;
+            if (num - arr[j].first < 0) {
+                continue;
+            }
+            if (map[num - arr[j].first] && map[num - arr[j].first] != arr[i].second && map[num - arr[j].first] != arr[j].second) {
+                std::cout << map[x - arr[i].first - arr[j].first] << " " << arr[i].second << " " << arr[j].second << "\n";
                 return 0;
             }
         }
     }
-    std::cout << "IMPOSSIBLE" << std::endl;
+    std::cout << "IMPOSSIBLE" << "\n";
     return 0;
 }
