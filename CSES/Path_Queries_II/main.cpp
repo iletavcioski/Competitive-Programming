2c79e93c8bbed4f0664f715f1feaab44b5ea6c9f
theteleton <it8816@student.uni-lj.si>
2024-11-16 10:50:23 +0100

    Kaizen day 6 -- Path Queries II solved

--- CSES/Path_Queries_II/main.cpp
+++ CSES/Path_Queries_II/main.cpp
@@ -3,7 +3,7 @@
 const int MIN = INT_MIN;
 const int N = 200000;
 
-int segment_tree[3 * N];
+int segment_tree[5 * N];
 std::vector<int> tree[N];
 int _index[N];
 int max_subtree[N];
@@ -12,14 +12,15 @@ int heavy_parent[N];
 int depth[N];
 int values[N];
 int n;
+int end_nodes[N];
 
 int I = -1;
 int J = -1;
 int POS = -1;
 int VAL = -1;
-void update_segment_tree(int l = 0, int r = n - 1, int p = 1) {
+void update_segment_tree(int l = 0, int r = n - 1, int p = 1, int buffer = 0) {
     if (l == r && l == POS) {
-        segment_tree[p] = VAL;
+        segment_tree[buffer + p] = VAL;
         return;
     }
 
@@ -27,22 +28,22 @@ void update_segment_tree(int l = 0, int r = n - 1, int p = 1) {
         return;
     }
 
-    update_segment_tree(l, (l + r) / 2, 2 * p);
-    update_segment_tree((l + r)  / 2 + 1, r, 2 * p + 1);
+    update_segment_tree(l, (l + r) / 2, 2 * p, buffer);
+    update_segment_tree((l + r)  / 2 + 1, r, 2 * p + 1, buffer);
 
-    segment_tree[p] = std::max(segment_tree[2 * p], segment_tree[2 * p + 1]);
+    segment_tree[buffer + p] = std::max(segment_tree[buffer + 2 * p], segment_tree[buffer + 2 * p + 1]);
 }
 
-int query(int l = 0, int r = n - 1, int p = 1) {
+int query(int l = 0, int r = n - 1, int p = 1, int buffer = 0) {
     if (l >= I && r <= J) {
-        return segment_tree[p];
+        return segment_tree[buffer + p];
     }
 
     if (l > J || r < I) {
         return MIN;
     }
 
-    return std::max(query(l, (l + r)  / 2, 2 * p), query((l + r) / 2 + 1, r, 2 * p + 1));
+    return std::max(query(l, (l + r)  / 2, 2 * p, buffer), query((l + r) / 2 + 1, r, 2 * p + 1, buffer));
 }
 
 
@@ -80,11 +81,11 @@ int total_nodes;
 void dfs_hld(int current_node, int previous_node, int heavy_node) {
     POS = total_nodes;
     VAL = values[current_node];
-    update_segment_tree();
     _index[current_node] = total_nodes++;
     heavy_parent[current_node] = heavy_node;
 
     if (max_subtree[current_node] == -1) {
+        end_nodes[heavy_node] = current_node;
         return;
     }
     if (max_subtree[current_node] != previous_node) {
@@ -113,7 +114,7 @@ int find_lca(int a, int b) {
         if (lca[a][i] != lca[b][i]) {
             a = lca[a][i];
             b = lca[b][i];
-        } 
+        }
     }
     if (a == b) {
         return a;
@@ -122,24 +123,28 @@ int find_lca(int a, int b) {
 }
 
 int query_path_hld(int node, int ancestor) {
-    
+
     int result = std::max(values[node], values[ancestor]);
     while (depth[node] > depth[ancestor]) {
-        
+
         if (heavy_parent[node] == node) {
             result = std::max(values[node], result);
             node = lca[node][0];
         } else if (depth[heavy_parent[node]] > depth[ancestor]) {
+            int start_node = heavy_parent[node];
+            int end_node = end_nodes[heavy_parent[node]];
             J = _index[node];
             I = _index[heavy_parent[node]];
-            result = std::max(query(), result);
+            result = std::max(query(_index[start_node], _index[end_node], 1, 4 * (_index[start_node] )), result);
             node = heavy_parent[node];
         } else {
+            int start_node = heavy_parent[node];
+            int end_node = end_nodes[heavy_parent[node]];
             J = _index[node];
             I = _index[ancestor];
-            result = std::max(query(), result);
+            result = std::max(query(_index[start_node], _index[end_node], 1, 4 * (_index[start_node] )), result);
             break;
-            node = heavy_parent[node];
+
         }
     }
     return result;
@@ -170,7 +175,15 @@ int main() {
     calculate_lca(n);
     dfs_hld(0, -1, 0);
 
-
+    for (int i = 0; i < n; i++) {
+        POS = _index[i];
+        VAL = values[i];
+        if (i ==2) {
+            bool cc=false;
+            //std::cout << heavy_parent[i] << " " << end_nodes[i] << std::endl;
+        }
+        update_segment_tree(_index[heavy_parent[i]], _index[end_nodes[heavy_parent[i]]], 1, 4 * (_index[heavy_parent[i]] ));
+    }
     for (int i = 0; i < q; i++) {
         int type;
         scanf("%d", &type);
@@ -181,7 +194,7 @@ int main() {
             node--;
             POS = _index[node];
             VAL = value;
-            update_segment_tree();
+            update_segment_tree(_index[heavy_parent[node]], _index[end_nodes[heavy_parent[node]]], 1, 4 * (_index[heavy_parent[node]] ));
             values[node] = value;
         } else {
             int a, b;
